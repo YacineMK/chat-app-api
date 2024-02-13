f7676a05ed5aedeaa389fadb35eaf40a72fd2836
YacineMK <merroukiyacine06@gmail.com>
2024-02-13 17:33:17 +0100

    add routes files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from "cors";
-import { dbs } from "./config/db.js";
-
+import { dbs } from "./database/db.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/",(req,res)=> {
     res.status(200).send("hello World")
