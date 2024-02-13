d6afafa92af62cfd8f8c57207f4956a4163f4d12
YacineMK <merroukiyacine06@gmail.com>
2024-02-13 16:28:28 +0100

    conect with db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+//import { app } from "../index.js";
+const port = process.env.PORT || 3000;
+
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL)
-    .then( 
-        console.log("db is connect")
-    )
+export const dbs = (app) => {
+ mongoose.connect(process.env.MONGO_URL)
+    .then( () => {
+        console.log("db is connect");
+        app.listen(port,()=>{
+            console.log("gg the server is running")
+        })
+    })
     .catch((err)=>{
         console.log(err)
     })
+}
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-import * as dotenv from "dotenv";
 import express from 'express';
 import cors from "cors";
+import { dbs } from "./config/db.js";
 
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/",(req,res)=> {
     res.status(200).send("hello World")
 })
 
-app.listen(port,()=>{
-    console.log("gg the server is running")
-})
+dbs(app);
