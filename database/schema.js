ebb2f298358eac91bfc3d55301d670b918b69a78
YacineMK <merroukiyacine06@gmail.com>
2024-02-14 21:53:13 +0100

    schema dbs

--- database/schema.js
+++ database/schema.js
@@ -1,3 +1,24 @@
 import mongoose from "mongoose";
 
-mongoose.Schema
+export const userSchema = mongoose.Schema({
+    fullName : {
+        type : String ,
+        required : true ,
+    },
+    userName : {
+        type : String ,
+        required : true ,
+        unique: true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    password : {
+        type : String ,
+        required : true ,
+        unique: true ,
+    },
+    
+})
