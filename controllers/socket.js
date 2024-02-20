6cd2435964cbe26a6f2d1aac1b7f36207770f1c4
YacineMK <merroukiyacine06@gmail.com>
2024-02-20 19:36:00 +0100

    finish

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,32 +1,33 @@
 const http = require('http');
 const socketio = require('socket.io');
 
-const wsPort = 3000 ;
+const wsPort = 3000;
 const wsServer = http.createServer();
 const io = socketio(wsServer);
 
 export const ws = () => {
 
-//front part to get message    
-const form = document.getElementsByTagName('form');
-form.addEventListener('submit',(e)=>{
-    e.preventdefault();
-    const msg = e.target.elements.input.value ;
-    
-})
+    //front part to get message    
+    const form = document.getElementsByTagName('form')[0]; 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+        const msgInput = e.target.elements.input.value; 
+        io.emit('message', msgInput); 
+        e.target.elements.input.value = ''; 
+    });
 
-io.on('connection', socket =>{
-    console.log('A user connected');
+    io.on('connection', socket => {
+        console.log('A user connected');
 
-    // Emitting to all clients except the one that initiated the connection
-    socket.broadcast.emit('message',"a user join chat")
+        // Emitting to all clients except the one that initiated the connection
+        socket.broadcast.emit('message', "A user joined the chat");
 
-    socket.on('disconnect',()=>{
-       io.emit('message',"user left chat") 
-    })
-})
+        socket.on('disconnect', () => {
+            io.emit('message', "A user left the chat");
+        });
+    });
 
-wsServer.listen(wsPort,()=>{
-    console.log(`ws sever are runnig in ${wsPort}`)
-})
-}
+    wsServer.listen(wsPort, () => {
+        console.log(`WebSocket server is running on port ${wsPort}`);
+    });
+};
